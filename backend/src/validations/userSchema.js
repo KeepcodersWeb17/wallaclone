c3de2ba22ed6bde6af3f05ea680ea7f7e049d1df
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-02 21:33:07 +0200

    Enhance user schema validation by trimming and normalizing input fields

--- backend/src/validations/userSchema.js
+++ backend/src/validations/userSchema.js
@@ -3,10 +3,17 @@ import z from "zod";
 const userSchema = z.object({
   username: z
     .string()
+    .trim()
+    .toLowerCase()
     .min(3, { message: "Username must be at least 3 characters long" }),
-  email: z.string().email({ message: "Invalid email address" }),
+  email: z
+    .string()
+    .trim()
+    .toLowerCase()
+    .email({ message: "Invalid email address" }),
   password: z
     .string()
+    .trim()
     .min(6, { message: "Password must be at least 6 characters long" }),
 });
 
