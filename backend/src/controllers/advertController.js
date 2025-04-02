6f74c64cea27aa974e5f31e457a08d158a13ae6a
Alejandro Hervás <alesepto15@gmail.com>
2025-04-02 21:05:25 +0200

    Added getAdvert to advertController.

--- backend/src/controllers/advertController.js
+++ backend/src/controllers/advertController.js
@@ -1,4 +1,5 @@
 import { Advert } from "../models/advertModel"
+import createHttpError from 'http-errors'
 
 export const createAdvert = async (req, res, next ) => {
     try {
@@ -17,6 +18,22 @@ export const createAdvert = async (req, res, next ) => {
         const savedAdvert = await newAdvert.save()
 
         res.status(201).json({message: 'Advert created', advert: savedAdvert})
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getAdvert = async (req, res, next) => {
+    try {
+        const { id } = req.params
+
+        const advert = await Advert.findById(id)
+
+        if (!advert) {
+            return next(createHttpError(404, 'Advert not found'))
+        }
+
+        res.json({ advert })
     } catch (error) {
         next(error)
     }
