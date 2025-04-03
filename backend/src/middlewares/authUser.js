47fb1d273c6d87455db64b6c2d49d3afe91e8f2d
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-03 14:54:32 +0200

    Fix cookie name for access token in authentication middleware

--- backend/src/middlewares/authUser.js
+++ backend/src/middlewares/authUser.js
@@ -3,7 +3,7 @@ export const isAuthenticated = (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  jwt.verify(req.cookies.access_token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(req.cookies.accessToken, process.env.JWT_SECRET, (err, user) => {
     if (err) {
       return res.status(403).json({ message: "Forbidden" });
     }
