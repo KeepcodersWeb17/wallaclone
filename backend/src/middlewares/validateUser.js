7c2ae8ea0f4f8f06c562b725d9c19bd91502e65a
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-03 13:40:16 +0200

    Refactor validation middleware to use a higher-order function

--- backend/src/middlewares/validateUser.js
+++ backend/src/middlewares/validateUser.js
@@ -1,10 +1,10 @@
 import z from "zod";
 import { userSchema, loginSchema } from "../validations/userSchema.js";
 
-export const validateUser = (req, res, next) => {
+const validate = (schema) => (req, res, next) => {
   try {
-    const validatedUser = userSchema.parse(req.body);
-    req.body = validatedUser;
+    const validatedData = schema.parse(req.body);
+    req.body = validatedData;
     next();
   } catch (error) {
     if (error instanceof z.ZodError) {
@@ -18,19 +18,5 @@ export const validateUser = (req, res, next) => {
   }
 };
 
-export const validateLogin = (req, res, next) => {
-  try {
-    const validateLogin = loginSchema.parse(req.body);
-    req.body = validateLogin;
-    next();
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      res.status(400).json({
-        error: error.errors.map((err) => ({
-          field: err.path[0],
-          message: err.message,
-        })),
-      });
-    }
-  }
-};
+export const validateUser = validate(userSchema);
+export const validateLogin = validate(loginSchema);
