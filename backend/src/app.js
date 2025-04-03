d7b2051641635850127183aa3f908370a6857f6c
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-03 16:22:07 +0200

    Add cookie-parser middleware and enable URL-encoded data parsing

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,19 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRouter.js";
 import usersRouter from "./routes/usersRouter.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(morgan("dev"));
 
 app.use(
   cors({
@@ -17,8 +24,6 @@ app.use(
     credentials: true,
   })
 );
-
-app.use(morgan("dev"));
 
 app.get("/test", (req, res) => {
   res.json({ test: "Servidor funcionando" });
