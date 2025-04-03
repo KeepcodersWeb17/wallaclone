fb4c4d45c6894da8f7d8c2c17e665be85bf551ab
codesthenos <codesthenos@gmail.com>
2025-04-03 23:04:54 +0200

    created isOwner middleware

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import Advert from "../models/Advert.js";
 
 export const isAuthenticated = (req, res, next) => {
   if (!req.cookies.accessToken) {
@@ -12,4 +13,41 @@ export const isAuthenticated = (req, res, next) => {
     req.user = user;
     next();
   });
+};
+
+export const isOwner = async (req, res, next) => {
+  const advertId = req.params.id;
+  const userId = req.user.id;
+
+  if (!advertId) {
+    const error = new Error("advertId not provided");
+    error.statusCode = 400;
+    next(error);
+    return;
+  }
+
+  if (!userId) {
+    const error = new Error("userId not provided");
+    error.statusCode = 400;
+    next(error);
+    return;
+  }
+
+  const advert = await Advert.findById(advertId);
+
+  if (!advert) {
+    const error = new Error("advert not found");
+    error.statusCode = 404;
+    next(error);
+    return;
+  }
+
+  if (advert.owner.toString() !== userId) {
+    const error = new Error("user not allowed to access this advert");
+    error.statusCode = 401;
+    next(error);
+    return;
+  }
+
+  next();
 };
