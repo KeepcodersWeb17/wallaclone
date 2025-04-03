e159fd71f8f1d4ea41e10efe6bf386eedce28384
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-03 14:36:27 +0200

    Implement login controller with JWT authentication

---
+++ backend/src/controllers/auth.js
@@ -0,0 +1,34 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+export const login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    const isPasswordCorrect = await user.comparePassword(password);
+
+    if (!user || !isPasswordCorrect) {
+      const error = new Error("Incorrect username or password");
+      error.status = 401;
+      next(error);
+      return;
+    }
+
+    const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("access_token", accessToken, {
+      httpOnly: true,
+      secure: false, // set to true in production
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({ accessToken });
+  } catch (error) {
+    next(error);
+  }
+};
