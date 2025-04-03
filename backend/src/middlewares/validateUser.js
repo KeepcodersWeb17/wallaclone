fd6b78d1c8196129d0f15fdb5561ceafdf40db9f
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-03 11:12:28 +0200

    Add login validation middleware using Zod schema

--- backend/src/middlewares/validateUser.js
+++ backend/src/middlewares/validateUser.js
@@ -1,10 +1,27 @@
 import z from "zod";
-import userSchema from "../validations/userSchema.js";
+import { userSchema, loginSchema } from "../validations/userSchema.js";
 
 export const validateUser = (req, res, next) => {
   try {
     const validatedUser = userSchema.parse(req.body);
     req.body = validatedUser;
+    next();
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      res.status(400).json({
+        error: error.errors.map((err) => ({
+          field: err.path[0],
+          message: err.message,
+        })),
+      });
+    }
+  }
+};
+
+export const validateLogin = (req, res, next) => {
+  try {
+    const validateLogin = loginSchema.parse(req.body);
+    req.body = validateLogin;
     next();
   } catch (error) {
     if (error instanceof z.ZodError) {
