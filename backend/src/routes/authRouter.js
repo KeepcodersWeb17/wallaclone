18fa2cdda1bc827482abaed2189dc7d9acf2ae9d
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-04 01:38:40 +0200

    Add logout and authentication middleware imports to authRouter

--- backend/src/routes/authRouter.js
+++ backend/src/routes/authRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { login } from "../controllers/authController.js";
+import { login, logout } from "../controllers/authController.js";
+import { isAuthenticated } from "../middlewares/authMiddleware.js";
 import { validateLogin as validateUser } from "../middlewares/validateMiddleware.js";
 
 const router = express.Router();
