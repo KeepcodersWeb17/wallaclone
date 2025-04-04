43a5bbd3277692a0f421424b7a2d2d0605fdd827
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-04 16:35:51 +0200

    Update API endpoint in App component and improve code formatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [test, setTest] = useState<string>('Loading...')
+  const [test, setTest] = useState<string>("Loading...");
 
-  useEffect(()=> {
-    fetch('https://api.wallaclone.codesthenos.duckdns.org/test') // Fetch de test al endpoint /test
-    .then(res => res.json())
-    .then(data => setTest(data.test))
-    .catch(err => {
-      console.error(err)
-      setTest('Error loading data')
-    })
-  }, [])
+  useEffect(() => {
+    fetch("https://api.wallaclone.keepcoders.duckdns.org/test") // Fetch de test al endpoint /test
+      .then((res) => res.json())
+      .then((data) => setTest(data.test))
+      .catch((err) => {
+        console.error(err);
+        setTest("Error loading data");
+      });
+  }, []);
 
   return (
     <>
@@ -20,7 +20,7 @@ function App() {
         <h1>{test}</h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
