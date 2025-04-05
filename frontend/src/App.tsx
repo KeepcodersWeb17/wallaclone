57a429afbfd2a06f431064eaf7696935a047b2cc
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-06 01:16:52 +0200

    Implement authentication logic with login and logout functionality in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import type State from "./store/state/types";
+import { AuthLogin, AuthLogout } from "./store/actions/creators";
 
 function App() {
   const [test, setTest] = useState<string>("Loading...");
+  const isLogged = useSelector((state: State) => state.auth);
+  const dispatch = useDispatch();
+
+  // Hook to manage the inputs state
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
     fetch("https://api.wallaclone.keepcoders.duckdns.org/test") // Fetch de test al endpoint /test
@@ -14,11 +23,53 @@ function App() {
       });
   }, []);
 
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const credentials = { username, password };
+    await dispatch(AuthLogin(credentials));
+  };
+
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogout = () => {
+    dispatch(AuthLogout());
+  };
+
   return (
     <>
-      <div>
-        <h1>{test}</h1>
-      </div>
+      <h1>{test}</h1>
+      {isLogged ? (
+        <>
+          <h2>Bienvenido {username}</h2>
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      ) : (
+        <form onSubmit={handleLogin}>
+          <h2>LOGIN</h2>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <button type="submit">Login</button>
+        </form>
+      )}
     </>
   );
 }
