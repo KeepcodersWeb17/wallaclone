db78344d72f1d4389d5b0cf4b5908bc939da2b68
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-06 02:05:23 +0200

    Set CORS mode for login fetch request

--- frontend/src/store/services/auth.ts
+++ frontend/src/store/services/auth.ts
@@ -9,6 +9,7 @@ export const login = async (credentials: Credentials) => {
       method: "POST",
       headers: { "content-type": "application/json" },
       credentials: "include",
+      mode: "cors",
       body: JSON.stringify(credentials),
     }
   );
