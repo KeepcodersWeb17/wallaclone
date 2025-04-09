391fa4441480b2536984d6b7c3a571488560f476
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-09 16:28:11 +0200

    Refactor AdvertPage component to improve advert fetching logic and state management

--- frontend/src/pages/AdvertPage.tsx
+++ frontend/src/pages/AdvertPage.tsx
@@ -1,28 +1,28 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import State from "../store/state/types";
+import { useDispatch } from "react-redux";
+import { getAdvert } from "../store/actions/creators";
 
 const AdvertPage = () => {
-  const adverts = useSelector((state: State) => state.adverts);
+  const dispatch = useDispatch();
 
   const { advert } = useParams();
 
   const advertId = advert ? advert.split("-")[1] : null;
 
-  if (!advertId) {
-    return <p>Advert not found.</p>;
-  }
+  // @ts-expect-error lo vamos a tipar mas adelante
+  dispatch(getAdvert(advertId));
 
-  const advertDetails = adverts.find((advert) => advert.id === advertId);
+  const advertDetails = useSelector((state: State) => state.advert);
 
   if (!advertDetails) {
-    return <p>Advert not found.</p>;
+    console.log("Advert not found");
+    return <p>Advert not found</p>;
   }
 
   return (
-    <>
-      <h2>{advertDetails.name}</h2>
-    </>
+    <>{!advertId ? <p>Advert not found</p> : <h2>{advertDetails.name}</h2>}</>
   );
 };
 
