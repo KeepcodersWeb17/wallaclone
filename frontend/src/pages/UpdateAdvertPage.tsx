93723fbdd59f79ac53bf6c97b0817aea4547ff5e
Alejandro Hervás <alesepto15@gmail.com>
2025-04-10 12:07:33 +0200

    Add UpdateAdvertPage component for updating advertisements

---
+++ frontend/src/pages/UpdateAdvertPage.tsx
@@ -0,0 +1,10 @@
+const UpdateAdvertPage = () => {
+  return (
+    <div>
+      <h1>Update Advert</h1>
+      <p>This is the Update Advert page.</p>
+    </div>
+  );
+};
+
+export default UpdateAdvertPage;
