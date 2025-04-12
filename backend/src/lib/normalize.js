6b3cc1e4e86a8c648b583081eb6a132296cf6710
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-12 17:39:38 +0200

    Add normalization functions for price and sorting in MongoDB queries

---
+++ backend/src/lib/normalize.js
@@ -0,0 +1,21 @@
+export const normalizePriceMongo = (priceString) => {
+  // return the normalized price filter
+  if (priceString.includes("-")) {
+    const priceList = priceString.split("-");
+
+    if (priceList[0] === "") return { $lte: +priceList[1] };
+
+    if (priceList[1] === "") return { $gte: +priceList[0] };
+
+    return { $gte: +priceList[0], $lte: +priceList[1] };
+  }
+  return +priceString;
+};
+
+export const normalizeSortMongo = (sortString) => {
+  // return normalized sort
+  if (sortString === "name-asc") return { name: 1 };
+  if (sortString === "name-desc") return { name: -1 };
+  if (sortString === "price-asc") return { price: 1 };
+  if (sortString === "price-desc") return { price: -1 };
+};
