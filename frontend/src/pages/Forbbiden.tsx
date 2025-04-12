bd7cf6bcd3806827384b7a953f79bb3c5deb48e8
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-12 16:12:12 +0200

    Add ForbbidenPage component

---
+++ frontend/src/pages/Forbbiden.tsx
@@ -0,0 +1,10 @@
+const ForbbidenPage = () => {
+  return (
+    <div>
+      <h2>403 - Forbbiden</h2>
+      <p>You do not have permission to access this page.</p>
+    </div>
+  );
+};
+
+export default ForbbidenPage;
