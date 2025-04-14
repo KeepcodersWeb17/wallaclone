b2cf3f363b3cb1a02ef9d7b7aa9f788543d3201a
Alejandro Hervás <alesepto15@gmail.com>
2025-04-14 14:28:56 +0200

    Add My Adverts Page component

---
+++ frontend/src/pages/MyAdvertsPage.tsx
@@ -0,0 +1,9 @@
+const myAdvertsPage = () => {
+    return (
+        <div>
+        <h1>My Adverts Page</h1>
+        <p>This is the My Adverts page.</p>
+        </div>
+    );
+    }
+export default myAdvertsPage;
