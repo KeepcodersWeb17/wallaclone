65b990d43c9f634a8cff57ff031996aa5bc95f71
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-14 22:02:02 +0200

    Apply Prettier formatting

--- frontend/src/lib/preloadedState.ts
+++ frontend/src/lib/preloadedState.ts
@@ -1,12 +1,12 @@
 const getUser = async () => {
   const response = await fetch(
     "https://api.wallaclone.keepcoders.duckdns.org/users",
-    { credentials: "include" }
+    { credentials: "include" },
   ).then((res) => res.json());
 
-  if(response.error) return null;
+  if (response.error) return null;
 
-  return response
+  return response;
 };
 
 const getPreloadedState = async () => {
