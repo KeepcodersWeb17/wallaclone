ea350ff1b54e10a3bdc579125a8ba2bd8df26d76
Alejandro Hervás <alesepto15@gmail.com>
2025-04-14 15:16:58 +0200

    Implement My Adverts Page with user authentication and adverts fetching

--- frontend/src/pages/MyAdvertsPage.tsx
+++ frontend/src/pages/MyAdvertsPage.tsx
@@ -1,4 +1,29 @@
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import State from "../store/state/types";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getAdverts } from "../store/actions/creators";
+
 const myAdvertsPage = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state: State) => state.user);
+    const adverts = useSelector((state: State) => state.adverts);
+
+    useEffect(() => {
+        if(!user.username) {
+            navigate("/login");
+        } else {
+            const queryString = new URLSearchParams({
+                username: user.username,
+            }).toString();
+
+            // @ts-expect-error Lo vamos a tipar más adelante
+            dispatch(getAdverts(queryString));
+
+        }
+    }, [user.username, dispatch]);
     return (
         <div>
         <h1>My Adverts Page</h1>
