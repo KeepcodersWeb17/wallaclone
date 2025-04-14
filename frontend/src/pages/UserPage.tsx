da985c863dae7f25c0b7e450b6302b369c8cede6
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-14 21:59:47 +0200

    Update UserPage component with delete account functionality and email masking

--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -1,39 +1,73 @@
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Link, Navigate, useParams } from "react-router-dom";
 import type State from "../store/state/types";
+import { deleteUser } from "../store/actions/creators";
 
 const UserPage = () => {
   const { username } = useParams();
 
-  const user = useSelector((state: State) => state.user);
+  const user = useSelector((state: State) => state.user)!;
 
-  const [openModal, setOpenModal] = useState(false);
+  const [isOpenDeleteAccountModal, setIsOpenDeleteAccountModal] =
+    useState(false);
+
+  const [isOpenSendAnEmailModal, setIsOpenSendAnEmailModal] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const maskedEmail = (email: string) => {
+    const [username, domainWithTLD] = email.split("@");
+    const [domain, ...tldParts] = domainWithTLD.split(".");
+    const maskedUser = username[0] + "***";
+    const maskedDomain = domain[0] + "***";
+    const tld = tldParts.join(".");
+    return `${maskedUser}@${maskedDomain}.${tld}`;
+  };
 
   const handleEdit = () => {
-    setOpenModal(true);
+    setIsOpenSendAnEmailModal(true);
   };
 
   const handleClose = () => {
-    setOpenModal(false);
+    setIsOpenSendAnEmailModal(false);
+    setIsOpenDeleteAccountModal(false);
   };
 
   const handleSendAEmail = () => {
     // crear logica para enviar un email
     alert("Email sent");
-    setOpenModal(false);
+    setIsOpenSendAnEmailModal(false);
+  };
+
+  const handleDelete = () => {
+    setIsOpenDeleteAccountModal(true);
+  };
+
+  const handleConfirmDelete = async () => {
+    // @ts-expect-error lo vamos a tipar mas adelante
+    await dispatch(deleteUser());
+    alert("Account deleted");
+    setIsOpenDeleteAccountModal(false);
   };
 
   return (
     <>
-      <dialog open={openModal}>
+      <dialog open={isOpenSendAnEmailModal}>
         <h3>Change password</h3>
         <p>We will send you an email with a link to change your password.</p>
         <button onClick={handleClose}>Close</button>
         <button onClick={handleSendAEmail}>Send a email</button>
       </dialog>
 
-      {username !== user.username ? (
+      <dialog open={isOpenDeleteAccountModal}>
+        <h3>Delete account</h3>
+        <p>Are you sure you want to delete your account?</p>
+        <button onClick={handleClose}>Close</button>
+        <button onClick={handleConfirmDelete}>Delete</button>
+      </dialog>
+
+      {username !== user?.username ? (
         <Navigate to={"/403"} />
       ) : (
         <>
@@ -43,10 +77,14 @@ const UserPage = () => {
           <h2>My profile</h2>
 
           <div>
+            <p>Member since {user.createdAt!.split("-")[0]}</p>
+          </div>
+
+          <div>
             <div>
               <div>
                 <p>Username</p>
-                <p>{user.username}</p>
+                <p>{user?.username}</p>
               </div>
               <div>
                 <button disabled>Edit</button>
@@ -54,7 +92,7 @@ const UserPage = () => {
             </div>
             <div>
               <p>Email</p>
-              <p>a*******@****.com</p>
+              <p>{user?.email && maskedEmail(user?.email)}</p>
             </div>
             <div>
               <button disabled>Edit</button>
@@ -66,6 +104,10 @@ const UserPage = () => {
             <div>
               <button onClick={handleEdit}>Edit</button>
             </div>
+          </div>
+
+          <div>
+            <button onClick={handleDelete}>Delete account</button>
           </div>
         </>
       )}
