6356b486cce874d91bc3e3e5c51a44a596a9300c
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-15 12:14:56 +0200

    Add filter form to AdvertsPage for improved search functionality

--- frontend/src/pages/AdvertsPage.tsx
+++ frontend/src/pages/AdvertsPage.tsx
@@ -1,11 +1,16 @@
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams, useSearchParams } from "react-router-dom";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Link,
+  useLocation,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 import type State from "../store/state/types";
 import { getAdverts } from "../store/actions/creators";
 
 const AdvertsPage = () => {
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const { username } = useParams();
 
@@ -16,7 +21,7 @@ const AdvertsPage = () => {
 
   const dispatch = useDispatch();
 
-  const pathname = window.location.pathname;
+  const { pathname } = useLocation();
 
   useEffect(() => {
     // searchParams.entries() devuelve un array de arrays con los pares clave-valor de los parametros de busqueda
@@ -51,8 +56,67 @@ const AdvertsPage = () => {
     dispatch(getAdverts(queryString));
   }, [dispatch, searchParams, username, user?.id, pathname]);
 
+  const handleFilterSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log(
+      Array.from(event.currentTarget.querySelectorAll("input")).map(
+        (element) => element.value,
+      ),
+    );
+
+    const filters = Array.from(
+      event.currentTarget.querySelectorAll("input"),
+    ).map((element) => element.value);
+
+    const params: { name?: string; price?: string } = {};
+
+    if (filters[0]) {
+      params.name = filters[0];
+    }
+
+    if (filters[1]) {
+      params.price = `${filters[1]}-`;
+    }
+
+    if (filters[2]) {
+      params.price = params.price
+        ? `${filters[1]}-${filters[2]}`
+        : `-${filters[2]}`;
+    }
+
+    setSearchParams(params);
+  };
+
   return (
     <>
+      {/* Filtros */}
+      <form onSubmit={handleFilterSubmit}>
+        <input type="text" name="advertName" placeholder="Advert name..." />
+        <input
+          type="number"
+          name="minPrice"
+          id="minPrice"
+          min={0}
+          placeholder="Min"
+        />
+        <input
+          type="number"
+          name="maxPrice"
+          id="maxPrice"
+          min={0}
+          placeholder="Max"
+        />
+        <select name="tags" id="tags">
+          <option value="work">Select a category</option>
+          <option value="work">Work</option>
+          <option value="lifestyle">Lifestyle</option>
+          <option value="motor">Motor</option>
+          <option value="mobile">Mobile</option>
+        </select>
+        <button type="submit">Filter</button>
+      </form>
+
       <h2>Adverts</h2>
 
       {adverts.length === 0 ? (
