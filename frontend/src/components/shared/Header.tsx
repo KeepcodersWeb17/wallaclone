fcec054a876acc963f4dce8534ec349f0ab0536f
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-15 11:12:08 +0200

    Refactor Header component to improve layout and include UserIcon for user representation

--- frontend/src/components/shared/Header.tsx
+++ frontend/src/components/shared/Header.tsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import type State from "../../store/state/types";
 import { useDispatch } from "react-redux";
 import { authLogout } from "../../store/actions/creators";
+import UserIcon from "../icons/user";
 
 const Header = () => {
   const user = useSelector((state: State) => state.user);
@@ -15,27 +16,56 @@ const Header = () => {
     await dispatch(authLogout());
     navigate("/login");
   };
-
+  //max-w-7xl items-center justify-between p-4
   return (
-    <header>
-      <Link to={"/adverts"}>
-        <h1>Wallaclone</h1>
-      </Link>
-      <nav>
-        {user?.username ? (
-          <>
-            <button onClick={handleLogout}>Logout</button>
-            <Link to={`/users/${user.username}`}>My profile</Link>
-            <Link to={`/adverts/user/${user.username}`}>My Adverts</Link>
-            <Link to={`/adverts/favorites/${user.username}`}>Favorites</Link>
-          </>
-        ) : (
-          <>
-            <Link to={"/login"}>Login</Link>
-            <Link to={"/signup"}>Signup</Link>
-          </>
-        )}
-      </nav>
+    <header className="fixed top-0 w-full bg-white text-gray-800 shadow-md">
+      <div className="mx-auto flex max-w-7xl items-center justify-between p-2">
+        <div>
+          <Link to={"/adverts"}>
+            <h1>Wallaclone</h1>
+          </Link>
+        </div>
+        <div>
+          {user?.username && (
+            <div className="relative flex items-center text-left">
+              <button className="text-black transition-all duration-1000 hover:scale-103 hover:text-gray-700">
+                <UserIcon />
+              </button>
+            </div>
+          )}
+          {/* 
+          ring-opacity-5 absolute right-0 mt-2 w-48 rounded-md bg-white py-1 shadow-lg ring-1 ring-black
+          */}
+          <div className="absolute right-0 mt-2 flex flex-col rounded bg-white py-1 shadow-lg ring-1 ring-black">
+            <nav className="flex flex-col space-x-8">
+              <Link
+                to={`/users/${user?.username}`}
+                className="block w-full px-4 py-2 text-center text-sm hover:bg-gray-100"
+              >
+                My profile
+              </Link>
+              <Link
+                to={`/adverts/user/${user?.username}`}
+                className="block w-full px-4 py-2 text-center text-sm hover:bg-gray-100"
+              >
+                My Adverts
+              </Link>
+              <Link
+                to={`/adverts/favorites/${user?.username}`}
+                className="block w-full px-4 py-2 text-center text-sm hover:bg-gray-100"
+              >
+                Favorites
+              </Link>
+              <button
+                onClick={handleLogout}
+                className="block px-4 py-2 text-sm hover:bg-gray-100"
+              >
+                Logout
+              </button>
+            </nav>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
