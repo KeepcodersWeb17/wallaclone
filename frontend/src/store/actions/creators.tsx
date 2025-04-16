0a118a9f30590bf8d96142f57ba11580bfeea78c
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-16 15:02:16 +0200

    Refactor action types and remove unused action definitions for improved clarity

--- frontend/src/store/actions/creators.tsx
+++ frontend/src/store/actions/creators.tsx
@@ -15,11 +15,16 @@ import {
 } from "../services/users";
 import { getAll as getAllTagsAPI } from "../services/tags";
 
-export const authLoginPending = () => ({
-  type: "AUTH_LOGIN_PENDING"
+export const actionPending = () => ({
+  type: "ACTION_PENDING"
 });
 
-export const authLoginFulfilled = (userData: User) => ({
+export const actionRejected = (error: string) => ({
+  type: "ACTION_REJECTED",
+  payload: error
+});
+
+const authLoginFulfilled = (userData: User) => ({
   type: "AUTH_LOGIN_FULFILLED",
   payload: {
     id: userData.id,
@@ -27,72 +32,48 @@ export const authLoginFulfilled = (userData: User) => ({
   }
 });
 
-export const authLoginRejected = (error: string) => ({
-  type: "AUTH_LOGIN_REJECTED",
-  payload: error
-});
-
 export const authLogin = (userData: User) => {
   // @ts-expect-error Lo vamos a tipar más adelante
   return async function (dispatch) {
     try {
+      // dispatch(actionPending());
       const { id, username } = await login(userData);
       dispatch(authLoginFulfilled({ id, username }));
     } catch (error) {
+      // dispatch(actionRejected(error.message));
       console.error(error);
     }
   };
 };
 
-export const authLogoutPending = () => ({
-  type: "AUTH_LOGOUT_PENDING"
-});
+export const createUser = (userData: User) => {
+  // @ts-expect-error Lo vamos a tipar más adelante
+  return async function (dispatch) {
+    try {
+      // dispatch(actionPending());
+      const { username, password } = userData;
+      await createUserAPI(userData);
+      dispatch(authLogin({ username, password }));
+    } catch (error) {
+      // dispatch(actionRejected(error.message));
+      console.error(error);
+    }
+  };
+};
 
-export const authLogoutFulfilled = () => ({
+const authLogoutFulfilled = () => ({
   type: "AUTH_LOGOUT_FULFILLED"
-});
-
-export const authLogoutRejected = (error: string) => ({
-  type: "AUTH_LOGOUT_REJECTED",
-  payload: error
 });
 
 export const authLogout = () => {
   // @ts-expect-error Lo vamos a tipar más adelante
   return async function (dispatch) {
     try {
+      // dispatch(actionPending());
       await logout();
       dispatch(authLogoutFulfilled());
     } catch (error) {
-      console.error(error);
-    }
-  };
-};
-
-export const createUserPending = () => ({
-  type: "CREATE_USER_PENDING"
-});
-
-export const createUserFulfilled = (createdUser: User) => ({
-  type: "CREATE_USER_FULFILLED",
-  payload: createdUser
-});
-
-export const createUserRejected = (error: string) => ({
-  type: "CREATE_USER_REJECTED",
-  payload: error
-});
-
-export const createUser = (userData: User) => {
-  // @ts-expect-error Lo vamos a tipar más adelante
-  return async function (dispatch) {
-    try {
-      //dispatch(createUserPending());
-      const { username, password } = userData;
-      await createUserAPI(userData);
-      dispatch(authLogin({ username, password }));
-    } catch (error) {
-      // dispatch(createUserRejected(error.message));
+      // dispatch(actionRejected(error.message));
       console.error(error);
     }
   };
@@ -102,187 +83,117 @@ export const deleteUser = () => {
   // @ts-expect-error Lo vamos a tipar más adelante
   return async function (dispatch) {
     try {
+      // dispatch(actionPending());
       await deleteUserAPI();
       dispatch(authLogout());
     } catch (error) {
+      // dispatch(actionRejected(error.message));
       console.error(error);
     }
   };
 };
-
-export const getAdvertsPending = () => ({
-  type: "GET_ADVERTS_PENDING"
-});
 
 export const getAdvertsFulfilled = (adverts: Advert[]) => ({
   type: "GET_ADVERTS_FULFILLED",
   payload: adverts
 });
 
-export const getAdvertsRejected = (error: string) => ({
-  type: "GET_ADVERTS_REJECTED",
-  payload: error
-});
-
 export const getAdverts = (queryString: string) => {
   // @ts-expect-error Lo vamos a tipar más adelante
   return async function (dispatch) {
     try {
+      // dispatch(actionPending());
       const adverts = await getLatest(queryString);
       dispatch(getAdvertsFulfilled(adverts));
     } catch (error) {
+      // dispatch(actionRejected(error.message));
       console.error(error);
     }
   };
 };
-
-export const getAdvertPending = () => ({
-  type: "GET_ADVERT_PENDING"
-});
-
-export const getAdvertFulfilled = (advert: Advert) => ({
-  type: "GET_ADVERT_FULFILLED",
-  payload: advert
-});
-
-export const getAdvertRejected = (error: string) => ({
-  type: "GET_ADVERT_REJECTED",
-  payload: error
-});
 
 export const getAdvert = (advertId: string) => {
   // @ts-expect-error Lo vamos a tipar más adelante
   return async function (dispatch) {
     try {
+      // dispatch(actionPending());
       const advert = await getById(advertId);
-      dispatch(getAdvertFulfilled(advert));
+      dispatch(getAdvertsFulfilled([advert]));
     } catch (error) {
+      // dispatch(actionRejected(error.message));
       console.error(error);
     }
   };
 };
-
-export const createAdvertPending = () => ({
-  type: "ADVERT_CREATED_PENDING"
-});
-
-export const createAdvertFulfilled = () => ({
-  type: "ADVERT_CREATED_FULFILLED"
-});
-
-export const createAdvertRejected = (error: string) => ({
-  type: "ADVERT_CREATED_REJECTED",
-  payload: error
-});
 
 export const createAdvert = (advert: Advert) => {
-  // @ts-expect-error Lo vamos a tipar más adelante
-  return async function (dispatch) {
+  return async function () {
     try {
+      // dispatch(actionPending());
       await createAdvertAPI(advert);
-      dispatch(createAdvertFulfilled()); // TODO:mejor dispatch successNotification
     } catch (error) {
+      // dispatch(actionRejected(error.message));
       console.error(error);
     }
   };
 };
-
-export const updateAdvertPending = () => ({
-  type: "UPDATE_ADVERT_PENDING"
-});
-
-export const updateAdvertFulfilled = () => ({
-  type: "UPDATE_ADVERT_FULFILLED"
-});
-
-export const updateAdvertRejected = (error: string) => ({
-  type: "UPDATE_ADVERT_REJECTED",
-  payload: error
-});
 
 export const updateAdvert = (advert: Advert) => {
-  // @ts-expect-error lo tipamos luego
-  return async function (dispatch) {
+  return async function () {
     try {
+      // dispatch(actionPending());
       await updateAdvertAPI(advert);
-      dispatch(updateAdvertFulfilled());
     } catch (error) {
+      // dispatch(actionRejected(error.message));
       console.error(error);
     }
   };
 };
-
-export const deleteAdvertPending = () => ({
-  type: "DELETE_ADVERT_PENDING"
-});
 
 export const deleteAdvertFulfilled = () => ({
   type: "DELETE_ADVERT_FULFILLED"
-});
-
-export const deleteAdvertRejected = (error: string) => ({
-  type: "DELETE_ADVERT_REJECTED",
-  payload: error
 });
 
 export const deleteAdvert = (advertId: string) => {
   // @ts-expect-error lo vamos a tipar mas adelante
   return async function (dispatch) {
     try {
+      // dispatch(actionPending());
       await deleteAdvertAPI(advertId);
       dispatch(deleteAdvertFulfilled());
     } catch (error) {
+      // dispatch(actionRejected(error.message));
       console.error(error);
     }
   };
 };
-
-export const toogleFavoritePending = () => ({
-  type: "TOOGLE_FAVORITE_PENDING"
-});
-
-export const toogleFavoriteFullfilled = () => ({
-  type: "TOOGLE_FAVORITE_FULFILLED"
-});
-
-export const toogleFavoriteRejected = (error: string) => ({
-  type: "TOOGLE_FAVORITE_REJECTED",
-  payload: error
-});
 
 export const toogleFavorite = (isFavorite: boolean, advertId: string) => {
-  // @ts-expect-error lo vamos a tipar mas adelante
-  return async function (dispatch) {
+  return async function () {
     try {
+      // dispatch(actionPending());
       await toogleFavoriteAPI(isFavorite, advertId);
-      dispatch(toogleFavoriteFullfilled());
     } catch (error) {
+      // dispatch(actionRejected(error.message));
       console.error(error);
     }
   };
 };
-
-export const getAllTagsPending = () => ({
-  type: "GET_TAGS_PENDING"
-});
 
 export const getAllTagsFulfilled = (tags: string[]) => ({
   type: "GET_TAGS_FULFILLED",
   payload: tags
 });
 
-export const getAllTagsRejected = (error: string) => ({
-  type: "GET_TAGS_REJECTED",
-  payload: error
-});
-
 export const getAllTags = () => {
   // @ts-expect-error lo vamos a tipar mas adelante
   return async function (dispatch) {
     try {
+      // dispatch(actionPending());
       const tags = await getAllTagsAPI();
       dispatch(getAllTagsFulfilled(tags));
     } catch (error) {
+      // dispatch(actionRejected(error.message));
       console.error(error);
     }
   };
--- frontend/src/store/actions/types.ts
+++ frontend/src/store/actions/types.ts
@@ -1,8 +1,4 @@
-import { Advert, User } from "../state/types";
-
-type AuthLoginPending = {
-  type: "AUTH_LOGIN_PENDING";
-};
+import { Advert } from "../state/types";
 
 type AuthLoginFulfilled = {
   type: "AUTH_LOGIN_FULFILLED";
@@ -12,40 +8,8 @@ type AuthLoginFulfilled = {
   };
 };
 
-type AuthLoginRejected = {
-  type: "AUTH_LOGIN_REJECTED";
-  payload: string;
-};
-
-type AuthLogoutPending = {
-  type: "AUTH_LOGOUT_PENDING";
-};
-
 type AuthLogoutFulfilled = {
   type: "AUTH_LOGOUT_FULFILLED";
-};
-
-type AuthLogoutRejected = {
-  type: "AUTH_LOGOUT_REJECTED";
-  payload: string;
-};
-
-type CreateUserPending = {
-  type: "CREATE_USER_PENDING";
-};
-
-type CreateUserFulfilled = {
-  type: "CREATE_USER_FULFILLED";
-  payload: User;
-};
-
-type CreateUserRejected = {
-  type: "CREATE_USER_REJECTED";
-  payload: string;
-};
-
-type GetAdvertsPending = {
-  type: "GET_ADVERTS_PENDING";
 };
 
 type GetAdvertsFulfilled = {
@@ -53,125 +17,15 @@ type GetAdvertsFulfilled = {
   payload: Advert[];
 };
 
-type GetAdvertsRejected = {
-  type: "GET_ADVERTS_REJECTED";
-  payload: string;
-};
-
-type GetAdvertPending = {
-  type: "GET_ADVERT_PENDING";
-};
-
-type GetAdvertFulfilled = {
-  type: "GET_ADVERT_FULFILLED";
-  payload: Advert;
-};
-
-type GetAdvertRejected = {
-  type: "GET_ADVERT_REJECTED";
-  payload: string;
-};
-
-type CreateAdvertPending = {
-  type: "ADVERT_CREATED_PENDING";
-};
-
-type CreateAdvertFulfilled = {
-  type: "ADVERT_CREATED_FULFILLED";
-  payload: Advert;
-};
-
-type CreateAdvertRejected = {
-  type: "ADVERT_CREATED_REJECTED";
-  payload: string;
-};
-
-type UpdateAdvertPending = {
-  type: "UPDATE_ADVERT_PENDING";
-};
-
-type UpdateAdvertFulfilled = {
-  type: "UPDATE_ADVERT_FULFILLED";
-  payload: Advert;
-};
-
-type UpdateAdvertRejected = {
-  type: "UPDATE_ADVERT_REJECTED";
-  payload: string;
-};
-
-type DeleteAdvertPending = {
-  type: "DELETE_ADVERT_PENDING";
-};
-
-type DeleteAdvertFulfilled = {
-  type: "DELETE_ADVERT_FULFILLED";
-  payload: Advert;
-};
-
-type DeleteAdvertRejected = {
-  type: "DELETE_ADVERT_REJECTED";
-  payload: string;
-};
-
-type ToogleFavoritePending = {
-  type: "TOOGLE_FAVORITE_PENDING";
-};
-
-type ToogleFavoriteFulfilled = {
-  type: "TOOGLE_FAVORITE_FULFILLED";
-  payload: Advert;
-};
-
-type ToogleFavoriteRejected = {
-  type: "TOOGLE_FAVORITE_REJECTED";
-  payload: string;
-};
-
-type GetTagsPending = {
-  type: "GET_TAGS_PENDING";
-};
-
 type GetTagsFulfilled = {
   type: "GET_TAGS_FULFILLED";
   payload: string[];
 };
 
-type GetTagsRejected = {
-  type: "GET_TAGS_REJECTED";
-  payload: string;
-};
-
 type Action =
-  | AuthLoginPending
   | AuthLoginFulfilled
-  | AuthLoginRejected
-  | AuthLogoutPending
   | AuthLogoutFulfilled
-  | AuthLogoutRejected
-  | CreateUserPending
-  | CreateUserFulfilled
-  | CreateUserRejected
-  | GetAdvertsPending
   | GetAdvertsFulfilled
-  | GetAdvertsRejected
-  | GetAdvertPending
-  | GetAdvertFulfilled
-  | GetAdvertRejected
-  | CreateAdvertPending
-  | CreateAdvertFulfilled
-  | CreateAdvertRejected
-  | UpdateAdvertPending
-  | UpdateAdvertFulfilled
-  | UpdateAdvertRejected
-  | DeleteAdvertPending
-  | DeleteAdvertFulfilled
-  | DeleteAdvertRejected
-  | ToogleFavoritePending
-  | ToogleFavoriteFulfilled
-  | ToogleFavoriteRejected
-  | GetTagsPending
-  | GetTagsFulfilled
-  | GetTagsRejected;
+  | GetTagsFulfilled;
 
 export default Action;
