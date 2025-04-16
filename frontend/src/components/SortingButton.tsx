7f15345caa705126eedb2648fb80407cc05ca6a0
codesthenos <codesthenos@gmail.com>
2025-04-16 23:48:15 +0200

    Add SortingButton component and integrate it into AdvertsPage for sorting functionality

---
+++ frontend/src/components/SortingButton.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const SortingButton = ({
+  setSearchParams,
+  queryParam
+}: {
+  setSearchParams: (queryParam: string) => void;
+  queryParam: string;
+}) => {
+  const [param, setParam] = useState(queryParam);
+
+  const handleClick = () => {
+    setSearchParams(param);
+    setParam((prevParam) =>
+      prevParam.includes("-asc")
+        ? prevParam.replace("-asc", "-desc")
+        : prevParam.replace("-desc", "-asc")
+    );
+  };
+  return (
+    <button type="button" onClick={handleClick} className="uppercase">
+      SORTING: {param}
+    </button>
+  );
+};
+
+export default SortingButton;
