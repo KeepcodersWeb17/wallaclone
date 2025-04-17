113e6e33455bd873524f0a59e75ba1b99f9b2582
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-17 13:36:38 +0200

    Fix formatting in SortingButton to improve readability and maintainability

--- frontend/src/components/SortingButton.tsx
+++ frontend/src/components/SortingButton.tsx
@@ -11,12 +11,14 @@ const SortingButton = ({
 
   const handleClick = () => {
     setSearchParams(param);
+
     setParam((prevParam) =>
       prevParam.includes("-asc")
         ? prevParam.replace("-asc", "-desc")
         : prevParam.replace("-desc", "-asc")
     );
   };
+
   return (
     <button type="button" onClick={handleClick} className="uppercase">
       SORTING: {param}
