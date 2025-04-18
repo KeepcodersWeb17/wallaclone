7fda65edca9161713cd4677f0d7d471dc890b48c
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-18 12:56:25 +0200

    Refactor SignupPage component to simplify user input handling; remove local state management and streamline form submission

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../store/store";
 import { createUser } from "../store/actions/creators";
@@ -9,92 +8,55 @@ const SignupPage = () => {
   const location = useLocation();
   const dispatch = useAppDispatch();
 
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-
-  const disabledButton = !username || !email || !password || !confirmPassword;
-
   const handleCreateUser = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const userData = { username, email, password, confirmPassword };
+    const username =
+      e.currentTarget.querySelector<HTMLInputElement>("#username");
+    const email = e.currentTarget.querySelector<HTMLInputElement>("#email");
+    const password =
+      e.currentTarget.querySelector<HTMLInputElement>("#password");
+    const confirmPassword =
+      e.currentTarget.querySelector<HTMLInputElement>("#confirmPassword");
+
+    if (!username || !email || !password || !confirmPassword) return;
+
+    const userData = {
+      username: username.value,
+      email: email.value,
+      password: password.value,
+      confirmPassword: confirmPassword.value
+    };
 
     dispatch(createUser(userData, navigate, location));
   };
 
-  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
-  };
-
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
-  const handleConfirmPasswordChange = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setConfirmPassword(e.target.value);
-  };
-
   return (
     <>
-      <h2>Signup</h2>
-      <form onSubmit={handleCreateUser}>
+      <h2 className="text-center">Signup</h2>
+      <form
+        className="mx-auto flex max-w-3xl flex-col items-center justify-center gap-5"
+        onSubmit={handleCreateUser}
+      >
         <div>
           <label htmlFor="username">Username: </label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
+          <input type="text" name="username" id="username" />
         </div>
         <div>
           <label htmlFor="email">Email: </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
-          />
+          <input type="email" name="email" id="email" />
         </div>
         <div>
           <label htmlFor="password">Password: </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
+          <input type="password" name="password" id="password" />
         </div>
         <div>
           <label htmlFor="confirmPassword">Confirm Password: </label>
-          <input
-            type="password"
-            name="confirmPassword"
-            id="confirmPassword"
-            value={confirmPassword}
-            onChange={handleConfirmPasswordChange}
-          />
+          <input type="password" name="confirmPassword" id="confirmPassword" />
         </div>
         <div>
           {error?.length && <p style={{ color: "red" }}>{error.join(", ")}</p>}
-          {loading ? (
-            <p>loading...</p>
-          ) : (
-            <button type="submit" disabled={disabledButton}>
-              Create
-            </button>
-          )}
+          {loading ? <p>loading...</p> : <button type="submit">Create</button>}
         </div>
       </form>
     </>
