eadde06085411413a075ed31a2ad8bce674e626b
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-24 19:56:38 +0200

    Refactor Footer component for improved layout and styling

--- frontend/src/components/shared/Footer.tsx
+++ frontend/src/components/shared/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="mx-auto flex max-w-3xl justify-around">
+    <footer className="flex w-full items-center justify-center gap-4 bg-red-50 pr-4 pl-4 leading-10">
       <p>&copy; {new Date().getFullYear()} Wallaclone</p>
       <p>All rights reserved.</p>
     </footer>
