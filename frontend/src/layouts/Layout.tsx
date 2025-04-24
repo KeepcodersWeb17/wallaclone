06e29ea02588cae443b3d4a783adb85a506351d4
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-24 15:34:50 +0200

    Remove unused state and effect in Layout component to simplify code

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,28 +1,13 @@
-import { useEffect, useState } from "react";
 import Footer from "../components/shared/Footer";
 import Header from "../components/shared/Header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const [test, setTest] = useState<string>("Loading...");
-
-  useEffect(() => {
-    fetch("https://api.wallaclone.keepcoders.duckdns.org/test") // Fetch de test al endpoint /test
-      .then((res) => res.json())
-      .then((data) => setTest(data.test))
-      .catch((err) => {
-        console.error(err);
-        setTest("Error loading data");
-      });
-  }, []);
   return (
-    <>
+    <div className="relative flex flex-col items-center justify-center">
       <Header />
-      <h1 className="text-xxl mt-10 text-center font-bold text-red-500">
-        {test}
-      </h1>
       <div className="grow">{children}</div>
       <Footer />
-    </>
+    </div>
   );
 };
 
