c97b9bd0f01d1e7c3c1f31f94fab15a0faec5cdf
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-24 15:46:59 +0200

    Refactor Header component to improve UI and menu functionality

--- frontend/src/components/shared/Header.tsx
+++ frontend/src/components/shared/Header.tsx
@@ -3,45 +3,112 @@ import { useAppDispatch, useAppSelector } from "../../store/store";
 import { getUser } from "../../store/selectors/selectors";
 import { authLogout } from "../../store/actions/creators";
 import UserIcon from "../icons/User";
+import CloseIcon from "../icons/Close";
+import { useState } from "react";
 
 const Header = () => {
   const user = useAppSelector(getUser);
 
   const dispatch = useAppDispatch();
+
   const navigate = useNavigate();
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const handleLogout = () => {
+    closeMenu();
     navigate("/adverts");
     dispatch(authLogout());
   };
 
+  const openMenu = () => {
+    setIsMenuOpen(true);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
-    <header className="w-full bg-white text-gray-800 shadow-md">
-      <div className="mx-auto flex max-w-7xl items-center justify-between p-2">
-        <div>
-          <Link to={"/adverts"}>
-            <h1>Wallaclone</h1>
-          </Link>
+    <>
+      {/* menu */}
+      {/* cambiamos etiqueta dialog por etiqueta div para poder mejorar la UI */}
+      {/* la etiqueta dialog tiene un elemento backdrop que evita que el elemento dialog ocupe el 100% */}
+      {isMenuOpen && (
+        <div
+          id="menu"
+          className="fixed top-0 right-0 bottom-0 left-0 z-100 bg-red-50"
+        >
+          <div className="flex items-center justify-end p-4">
+            <button className="cursor-pointer" onClick={closeMenu}>
+              <CloseIcon />
+            </button>
+          </div>
+          <ul className="line-hi text-center text-3xl leading-24">
+            <Link to={`/users/${user?.username}`} onClick={closeMenu}>
+              <li className="hover:bg-gray-100">My profile</li>
+            </Link>
+            <Link to={`/adverts/user/${user?.username}`} onClick={closeMenu}>
+              <li className="hover:bg-gray-100">My Adverts</li>
+            </Link>
+            <Link
+              to={`/adverts/favorites/${user?.username}`}
+              onClick={closeMenu}
+            >
+              <li className="text-center hover:bg-gray-100">Favorites</li>
+            </Link>
+            <li
+              className="cursor-pointer text-center hover:bg-gray-100"
+              onClick={handleLogout}
+            >
+              Logout
+            </li>
+          </ul>
         </div>
-        <div>
-          {user?.username && (
-            <div className="relative flex items-center text-left">
-              <button className="text-black transition-all duration-1000 hover:scale-103 hover:text-gray-700">
-                <UserIcon />
-              </button>
-            </div>
-          )}
-          <div className="absolute right-0 mt-2 flex flex-col rounded bg-white py-1 shadow-lg ring-1 ring-black">
-            <nav className="flex flex-col space-x-8">
-              <Link
-                to={"/signup"}
-                className="block w-full px-4 py-2 text-center text-sm hover:bg-gray-100"
-                state={{ from: location.pathname }}
-                replace
-              >
-                Sign up
-              </Link>
-              <Link
+      )}
+
+      <header className="w-full bg-red-50">
+        <div className="mx-auto flex max-w-7xl items-center justify-between p-2">
+          <div>
+            <Link to={"/adverts"} state={{ from: location.pathname }} replace>
+              <h1>Wallaclone</h1>
+            </Link>
+          </div>
+
+          <nav>
+            {user?.username ? (
+              <ul>
+                <li className="flex items-center justify-center gap-4">
+                  <button onClick={openMenu} className="cursor-pointer">
+                    <UserIcon />
+                  </button>
+                </li>
+              </ul>
+            ) : (
+              <ul className="flex items-center justify-around gap-4">
+                <Link
+                  to={"/signup"}
+                  state={{ from: location.pathname }}
+                  replace
+                >
+                  <li className="btn btn-primary">Sign up</li>
+                </Link>
+                <Link to={"/login"} state={{ from: location.pathname }} replace>
+                  <li className="btn btn-primary">Log in</li>
+                </Link>
+              </ul>
+            )}
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
+
+{
+  /* <Link
                 to={`/users/${user?.username}`}
                 className="block w-full px-4 py-2 text-center text-sm hover:bg-gray-100"
               >
@@ -64,13 +131,5 @@ const Header = () => {
                 className="block cursor-pointer px-4 py-2 text-sm hover:bg-gray-100"
               >
                 Logout
-              </button>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
-};
-
-export default Header;
+              </button> */
+}
