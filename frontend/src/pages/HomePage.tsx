bd1c6dfe366dab4df348cdd83919bacc8ff2c9ff
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-25 11:45:01 +0200

    Refactor HomePage component to enhance search functionality and improve layout

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import SearchIcon from "../components/icons/Search";
 import { useEffect, useState } from "react";
 import { Advert } from "../store/state/types";
@@ -6,6 +6,8 @@ import { Advert } from "../store/state/types";
 const HomePage = () => {
   const [adverts, setAdverts] = useState<Advert[]>([]);
   const [tags, setTags] = useState<{ id: string; name: string }[]>([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchAdverts = async () => {
@@ -63,147 +65,145 @@ const HomePage = () => {
     });
   }, []);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const searchByAdvertName = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const advertName = event.currentTarget.advert.value;
+
+    navigate(`/adverts?name=${advertName}`);
   };
 
-  /*
-    const handleFilterSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-  
-      const filters = Array.from(
-        event.currentTarget.querySelectorAll("input")
-      ).map((element) => element.value);
-  
-      const tagsFilters = Array.from(
-        dialogRef.current!.querySelectorAll("li[selected]")
-      )
-        .map((element) => element.textContent)
-        .join("-")
-        .toLowerCase();
-  
-      const params = getParamsFilters({ filters, tagsFilters, searchParams });
-  
-      setSearchParams(params);
-    };  
-  */
+  const searchByCategory = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const tagName = event.currentTarget.textContent?.toLowerCase() || "";
+
+    navigate(`/adverts?tags=${tagName}`);
+  };
 
   return (
     <>
       <div className="flex flex-col items-center justify-center gap-2">
-        <h2 className="text-center"> What are you looking for? </h2>
-        <div>
-          <form className="flex gap-2" onSubmit={handleSubmit}>
-            <input
-              className="border"
-              type="text"
-              name="advert"
-              id="advert"
-              placeholder="Search..."
-            />
-            <button className="h-10" type="submit">
-              <SearchIcon />
-            </button>
-          </form>
-        </div>
-      </div>
+        {/* Search form */}
+        <section className="flex flex-col items-center justify-center gap-2">
+          <h2 className="text-center"> What are you looking for? </h2>
+          <div>
+            <form className="flex gap-2" onSubmit={searchByAdvertName}>
+              <input
+                className="border"
+                type="text"
+                name="advert"
+                id="advert"
+                placeholder="Search..."
+              />
+              <button className="h-10" type="submit">
+                <SearchIcon />
+              </button>
+            </form>
+          </div>
+        </section>
 
-      {/* Carrousel categories */}
-      <div>
-        <p>Categories</p>
-        {tags.length === 0 ? (
-          <p>No categories</p>
-        ) : (
-          <ul className="flex gap-4 overflow-x-auto scroll-smooth p-4">
-            {tags.map((tag) => (
-              <li key={tag.id}>
-                <Link to={`/adverts/${tag.name}`}>{tag.name}</Link>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-
-      {/* Carrousel latest ads for sale */}
-      <div>
-        <p>Lates ads for sale</p>
-        {adverts.length === 0 ? (
-          <p> No adverts </p>
-        ) : (
-          <ul className="flex snap-x gap-4 overflow-x-auto scroll-smooth p-4">
-            {adverts
-              .filter((advert) => advert.sale === "sell")
-              .map((advert) => (
-                <li key={`${advert.name}-${advert.id}`} className="">
-                  <Link to={`/adverts/${advert.name}-${advert.id}`}>
-                    {/* //TODO conviene usar figure? */}
-                    <figure>
-                      <picture>
-                        <source srcSet={advert.image} type="image/webp" />
-                        <source srcSet={advert.image} type="image/jpeg" />{" "}
-                        <img
-                          src={advert.image}
-                          alt={advert.name}
-                          className="object-cover"
-                        />
-                      </picture>
-                    </figure>
-                    <div>
-                      <h3>{advert.name}</h3>
-                      <p>{advert.description}</p>
-                      <p>Price: {advert.price}</p>
-                      {/* //TODO esto se puede mejorar */}
-                      <p>{advert.sale === "sell" ? "For sell" : "To buy"}</p>
-                    </div>
-                  </Link>
-                  <Link to={`/adverts/user/${advert.owner?.username}`}>
-                    Published by: {advert.owner?.username}
-                  </Link>
+        {/* Carrousel categories */}
+        <section className="">
+          <p>Categories</p>
+          {tags.length === 0 ? (
+            <p>No categories</p>
+          ) : (
+            <ul className="flex gap-4 overflow-x-auto scroll-smooth p-4">
+              {tags.map((tag) => (
+                <li key={tag.id}>
+                  <button onClick={searchByCategory}>{tag.name}</button>
                 </li>
               ))}
-          </ul>
-        )}
-      </div>
+            </ul>
+          )}
+        </section>
 
-      {/* Carrousel latest ads to buy */}
-      <div>
-        <p>People Are Looking For...</p>
-        {adverts.length === 0 ? (
-          <p> No adverts </p>
-        ) : (
-          <ul className="flex snap-x gap-4 overflow-x-auto scroll-smooth p-4">
-            {adverts
-              .filter((advert) => advert.sale === "buy")
-              .map((advert) => (
-                <li key={`${advert.name}-${advert.id}`} className="">
-                  <Link to={`/adverts/${advert.name}-${advert.id}`}>
-                    {/* //TODO conviene usar figure? */}
-                    <figure>
-                      <picture>
-                        <source srcSet={advert.image} type="image/webp" />
-                        <source srcSet={advert.image} type="image/jpeg" />{" "}
-                        <img
-                          src={advert.image}
-                          alt={advert.name}
-                          className="object-cover"
-                        />
-                      </picture>
-                    </figure>
-                    <div>
-                      <h3>{advert.name}</h3>
-                      <p>{advert.description}</p>
-                      <p>Price: {advert.price}</p>
-                      {/* //TODO esto se puede mejorar */}
-                      <p>{advert.sale === "sell" ? "For sell" : "To buy"}</p>
-                    </div>
-                  </Link>
-                  <Link to={`/adverts/user/${advert.owner?.username}`}>
-                    Published by: {advert.owner?.username}
-                  </Link>
-                </li>
-              ))}
-          </ul>
-        )}
+        {/* Carrousel latest ads for sale */}
+        <section>
+          <p>Lates ads for sale</p>
+          {adverts.length === 0 ? (
+            <p> No adverts </p>
+          ) : (
+            <ul className="flex snap-x gap-4 overflow-x-auto scroll-smooth p-4">
+              {adverts
+                .filter((advert) => advert.sale === "sell")
+                .map((advert) => (
+                  <li key={`${advert.name}-${advert.id}`} className="">
+                    <Link to={`/adverts/${advert.name}-${advert.id}`}>
+                      {/* //TODO conviene usar figure? */}
+                      <figure>
+                        <picture>
+                          <source srcSet={advert.image} type="image/webp" />
+                          <source
+                            srcSet={advert.image}
+                            type="image/jpeg"
+                          />{" "}
+                          <img
+                            src={advert.image}
+                            alt={advert.name}
+                            className="object-cover"
+                          />
+                        </picture>
+                      </figure>
+                      <div>
+                        <h3>{advert.name}</h3>
+                        <p>{advert.description}</p>
+                        <p>Price: {advert.price}</p>
+                        {/* //TODO esto se puede mejorar */}
+                        <p>{advert.sale === "sell" ? "For sell" : "To buy"}</p>
+                      </div>
+                    </Link>
+                    <Link to={`/adverts/user/${advert.owner?.username}`}>
+                      Published by: {advert.owner?.username}
+                    </Link>
+                  </li>
+                ))}
+            </ul>
+          )}
+        </section>
+
+        {/* Carrousel latest ads to buy */}
+        <section>
+          <p>People Are Looking For...</p>
+          {adverts.length === 0 ? (
+            <p> No adverts </p>
+          ) : (
+            <ul className="flex snap-x gap-4 overflow-x-auto scroll-smooth p-4">
+              {adverts
+                .filter((advert) => advert.sale === "buy")
+                .map((advert) => (
+                  <li key={`${advert.name}-${advert.id}`} className="">
+                    <Link to={`/adverts/${advert.name}-${advert.id}`}>
+                      {/* //TODO conviene usar figure? */}
+                      <figure>
+                        <picture>
+                          <source srcSet={advert.image} type="image/webp" />
+                          <source
+                            srcSet={advert.image}
+                            type="image/jpeg"
+                          />{" "}
+                          <img
+                            src={advert.image}
+                            alt={advert.name}
+                            className="object-cover"
+                          />
+                        </picture>
+                      </figure>
+                      <div>
+                        <h3>{advert.name}</h3>
+                        <p>{advert.description}</p>
+                        <p>Price: {advert.price}</p>
+                        {/* //TODO esto se puede mejorar */}
+                        <p>{advert.sale === "sell" ? "For sell" : "To buy"}</p>
+                      </div>
+                    </Link>
+                    <Link to={`/adverts/user/${advert.owner?.username}`}>
+                      Published by: {advert.owner?.username}
+                    </Link>
+                  </li>
+                ))}
+            </ul>
+          )}
+        </section>
       </div>
     </>
   );
