0213a5c0aed6799293efe6b4e240fcd942e4bc19
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-25 14:23:38 +0200

    Refactor Layout component to simplify padding on children container

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative flex min-h-screen flex-col bg-red-200">
       <Header />
-      <div className="grow pr-4 pl-4">{children}</div>
+      <div className="grow p-4">{children}</div>
       <Footer />
     </div>
   );
