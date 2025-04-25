e89227bfb9b1bc1c79c5c4b9971636e94022fb5c
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-25 09:32:20 +0200

    Refactor Layout component to enhance structure and layout flexibility

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -3,7 +3,7 @@ import Header from "../components/shared/Header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="relative min-h-screen bg-red-200">
+    <div className="relative flex min-h-screen flex-col bg-red-200">
       <Header />
       <div className="grow pr-4 pl-4">{children}</div>
       <Footer />
