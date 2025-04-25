9d79827bfcbb9a3abacc2ac060be8c5b2e9f8834
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-25 11:20:39 +0200

    Refactor AdvertsPage layout and improve filtering, sorting, and pagination UI

--- frontend/src/pages/AdvertsPage.tsx
+++ frontend/src/pages/AdvertsPage.tsx
@@ -126,99 +126,113 @@ const AdvertsPage = () => {
 
   return (
     <>
-      {/* Modal Cateogries */}
-      <TagsDiaglog
-        ref={dialogRef}
-        handleClose={handleClose}
-        handleSelected={handleSelected}
-      />
+      <div className="bg-red-600">
+        {/* Modal Cateogries */}
+        <TagsDiaglog
+          ref={dialogRef}
+          handleClose={handleClose}
+          handleSelected={handleSelected}
+        />
 
-      {/* Filtros */}
-      {error && <p className="text-red-500">{error.join(", ")}</p>}
-      <form onSubmit={handleFilterSubmit}>
-        <input type="text" name="advertName" placeholder="Advert name..." />
-        <input
-          type="number"
-          name="minPrice"
-          id="minPrice"
-          min={0}
-          placeholder="Min"
-        />
-        <input
-          type="number"
-          name="maxPrice"
-          id="maxPrice"
-          min={0}
-          placeholder="Max"
-        />
-        <button type="button" onClick={handleOpenModal}>
-          Category
-        </button>
-        {loading ? <p>Loading...</p> : <button type="submit">Filter</button>}
-      </form>
+        {/* Filtros */}
+        {error && <p className="text-red-500">{error.join(", ")}</p>}
+        <form className="bg-red-400" onSubmit={handleFilterSubmit}>
+          <input type="text" name="advertName" placeholder="Advert name..." />
+          <input
+            type="number"
+            name="minPrice"
+            id="minPrice"
+            min={0}
+            placeholder="Min"
+          />
+          <input
+            type="number"
+            name="maxPrice"
+            id="maxPrice"
+            min={0}
+            placeholder="Max"
+          />
+          <button type="button" onClick={handleOpenModal}>
+            Category
+          </button>
+          {loading ? <p>Loading...</p> : <button type="submit">Filter</button>}
+        </form>
 
-      {/* Filter by user */}
-      <ShowUserAdverts />
+        {/* Filter by user */}
+        <ShowUserAdverts />
 
-      {/* Sorting */}
-      <div className="flex justify-center gap-5">
-        <SortingButton
-          queryParam="date-asc"
-          setSearchParams={handleSortParamsClick}
-        />
-        <SortingButton
-          queryParam="name-asc"
-          setSearchParams={handleSortParamsClick}
-        />
-        <SortingButton
-          queryParam="price-desc"
-          setSearchParams={handleSortParamsClick}
-        />
+        {/* Sorting */}
+        <div className="flex justify-center gap-5">
+          <SortingButton
+            queryParam="date-asc"
+            setSearchParams={handleSortParamsClick}
+          />
+          <SortingButton
+            queryParam="name-asc"
+            setSearchParams={handleSortParamsClick}
+          />
+          <SortingButton
+            queryParam="price-desc"
+            setSearchParams={handleSortParamsClick}
+          />
+        </div>
+
+        {/* Paginacion */}
+        <div className="flex justify-center gap-5">
+          <button type="button" onClick={handlePrevPage}>
+            Previous
+          </button>
+          <button type="button" onClick={handleNextPage}>
+            Next
+          </button>
+          <input
+            type="number"
+            step={2}
+            min={2}
+            max={10}
+            onKeyDown={handleLimit}
+            placeholder="Limit"
+          />
+        </div>
+
+        <section>
+          <p>Lates ads published</p>
+          {adverts.list.length === 0 ? (
+            <p> No adverts </p>
+          ) : (
+            <ul className="">
+              {adverts.list.map((advert) => (
+                <li key={`${advert.name}-${advert.id}`} className="">
+                  <Link to={`/adverts/${advert.name}-${advert.id}`}>
+                    {/* //TODO conviene usar figure? */}
+                    <figure>
+                      <picture>
+                        <source srcSet={advert.image} type="image/webp" />
+                        <source srcSet={advert.image} type="image/jpeg" />{" "}
+                        <img
+                          src={advert.image}
+                          alt={advert.name}
+                          className="object-cover"
+                        />
+                      </picture>
+                    </figure>
+                    <div>
+                      <h3>{advert.name}</h3>
+                      <p>{advert.description}</p>
+                      <p>Price: {advert.price}</p>
+                      {/* //TODO esto se puede mejorar */}
+                      <p>{advert.sale === "sell" ? "For sell" : "To buy"}</p>
+                    </div>
+                  </Link>
+                  <Link to={`/adverts/user/${advert.owner?.username}`}>
+                    Published by: {advert.owner?.username}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
-
-      {/* Paginacion */}
-      <div className="flex justify-center gap-5">
-        <button type="button" onClick={handlePrevPage}>
-          Previous
-        </button>
-        <button type="button" onClick={handleNextPage}>
-          Next
-        </button>
-        <input
-          type="number"
-          step={2}
-          min={2}
-          max={10}
-          onKeyDown={handleLimit}
-          placeholder="Limit"
-        />
-      </div>
-
-      <h2>Adverts</h2>
-      {adverts.list.length === 0 ? (
-        <p> No adverts </p>
-      ) : (
-        <ul>
-          {adverts.list.map((advert) => (
-            <li key={`${advert.name}-${advert.id}`}>
-              <Link to={`/adverts/${advert.name}-${advert.id}`}>
-                <div>
-                  <img src={advert.image} alt={advert.name} />
-                </div>
-                <div>
-                  <h3>{advert.name}</h3>
-                  <p>{advert.description}</p>
-                  <p>Price: {advert.price}</p>
-                  <p>On: {advert.sale}</p>
-                </div>
-              </Link>
-              <Link to={`/adverts/user/${advert.owner?.username}`}>
-                Owner: {advert.owner?.username}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
     </>
   );
 };
