db545a6c8ff357b207998010b87716b24592e296
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-27 00:34:50 +0200

    Refactor TagsDialog component for improved layout and styling

--- frontend/src/components/TagsDialog.tsx
+++ frontend/src/components/TagsDialog.tsx
@@ -18,29 +18,38 @@ const TagsDiaglog = ({ handleClose, handleSelected, ref }: Props) => {
   }, [dispatch]);
 
   return (
-    <dialog ref={ref} className="h-full w-full">
-      <button
-        className="absolute top-5 right-5 cursor-pointer"
-        onClick={handleClose}
-      >
-        <CloseIcon />
-      </button>
-      <h2 className="">Categories</h2>
-      <ul className="flex w-full flex-col gap-5 text-center">
-        {tags.map((tag) => (
-          <li
-            id={tag.id}
-            key={tag.id}
-            className="cursor-pointer rounded hover:bg-gray-100"
-            onClick={handleSelected}
-          >
-            {tag.name}
-          </li>
-        ))}
-      </ul>
-      <button className="cursor-pointer" onClick={handleClose}>
-        Confirm
-      </button>
+    <dialog
+      ref={ref}
+      className="absolute top-1/2 left-1/2 z-200 h-1/2 w-1/2 -translate-x-1/2 -translate-y-1/2 rounded bg-white p-5 shadow-lg"
+    >
+      <div className="flex w-full flex-col items-center justify-between gap-4">
+        <div className="flex w-full flex-row items-center justify-between">
+          <h3 className="text-lg leading-10 font-bold">Categories</h3>
+          <button className="cursor-pointer" onClick={handleClose}>
+            <CloseIcon />
+          </button>
+        </div>
+
+        <ul className="flex w-full flex-col gap-1 text-center">
+          {tags.map((tag) => (
+            <li
+              id={tag.id}
+              key={tag.id}
+              className="cursor-pointer rounded leading-8 capitalize hover:bg-gray-100"
+              onClick={handleSelected}
+            >
+              {tag.name}
+            </li>
+          ))}
+        </ul>
+
+        <button
+          className="w-full cursor-pointer rounded-lg border border-gray-400 px-5 py-1.5 text-xs text-gray-400 hover:bg-black hover:text-white"
+          onClick={handleClose}
+        >
+          Confirm
+        </button>
+      </div>
     </dialog>
   );
 };
