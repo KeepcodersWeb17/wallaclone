ad3b88b50f95399bed8e51a1d7598894412d1221
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-28 00:40:03 +0200

    Enhance Layout component with responsive padding and width for improved layout consistency

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -5,7 +5,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative flex min-h-screen flex-col bg-gray-300">
       <Header />
-      <div className="grow p-4">{children}</div>
+      <div className="grow p-4 sm:mx-auto sm:w-7/8 md:mx-auto md:w-3/4">
+        {children}
+      </div>
       <Footer />
     </div>
   );
