fea9731efc2eb719b5b8820fa4634024e6c70bc1
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-28 12:50:44 +0200

    Enhance Like and Unlike icons with drop shadow for improved visual appeal

--- frontend/src/components/icons/Unlike.tsx
+++ frontend/src/components/icons/Unlike.tsx
@@ -2,11 +2,11 @@ const UnlikeIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
+      fill="white"
       viewBox="0 0 24 24"
-      strokeWidth="1.5"
+      strokeWidth="1"
       stroke="currentColor"
-      className="size-6"
+      className="size-6 drop-shadow-lg"
     >
       <path
         strokeLinecap="round"
