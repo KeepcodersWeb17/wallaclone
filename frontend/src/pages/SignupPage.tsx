4189c3fa53a9d48ca6e5ec406166fcfbaadb091f
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-28 22:58:10 +0200

    Refactor SignupPage layout for improved styling and user experience

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -35,34 +35,74 @@ const SignupPage = () => {
   };
 
   return (
-    <>
-      <h2 className="text-center">Signup</h2>
-      <form
-        className="mx-auto flex max-w-3xl flex-col items-center justify-center gap-5"
-        onSubmit={handleCreateUser}
-      >
-        <div>
-          <label htmlFor="username">Username: </label>
-          <input type="text" name="username" id="username" />
-        </div>
-        <div>
-          <label htmlFor="email">Email: </label>
-          <input type="email" name="email" id="email" />
-        </div>
-        <div>
-          <label htmlFor="password">Password: </label>
-          <input type="password" name="password" id="password" />
-        </div>
-        <div>
-          <label htmlFor="confirmPassword">Confirm Password: </label>
-          <input type="password" name="confirmPassword" id="confirmPassword" />
-        </div>
-        <div>
-          {error?.length && <p style={{ color: "red" }}>{error.join(", ")}</p>}
-          {loading ? <p>loading...</p> : <button type="submit">Create</button>}
-        </div>
-      </form>
-    </>
+    <div className="grid w-full grid-cols-1 place-items-center">
+      <div className="flex w-full max-w-125 flex-col rounded-lg border border-gray-400 p-4 sm:w-2/3">
+        <h2 className="mb-5 text-center text-lg leading-10 font-semibold text-gray-500 sm:text-xl">
+          Create your account
+        </h2>
+        <form
+          className="flex w-full flex-col gap-5"
+          onSubmit={handleCreateUser}
+        >
+          <div className="flex flex-col justify-between">
+            <input
+              className="h-10 w-full rounded-lg border border-gray-400 p-1 px-4 placeholder:text-xs placeholder:text-gray-500 placeholder:italic focus:ring-1 focus:ring-gray-500 focus:outline-none sm:placeholder:text-sm"
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Enter your username"
+              required
+              minLength={3}
+            />
+          </div>
+          <div className="flex flex-col justify-between">
+            <input
+              className="h-10 w-full rounded-lg border border-gray-400 p-1 px-4 placeholder:text-xs placeholder:text-gray-500 placeholder:italic focus:ring-1 focus:ring-gray-500 focus:outline-none sm:placeholder:text-sm"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div className="flex flex-col justify-between">
+            <input
+              className="h-10 w-full rounded-lg border border-gray-400 p-1 px-4 placeholder:text-xs placeholder:text-gray-500 placeholder:italic focus:ring-1 focus:ring-gray-500 focus:outline-none sm:placeholder:text-sm"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <div className="flex flex-col justify-between">
+            <input
+              className="h-10 w-full rounded-lg border border-gray-400 p-1 px-4 placeholder:text-xs placeholder:text-gray-500 placeholder:italic focus:ring-1 focus:ring-gray-500 focus:outline-none sm:placeholder:text-sm"
+              type="password"
+              name="confirmPassword"
+              id="confirmPassword"
+              placeholder="Confirm your password"
+              required
+            />
+          </div>
+          <div className="mt-5 w-full">
+            {error?.length && (
+              <p style={{ color: "red" }}>{error.join(", ")}</p>
+            )}
+            {loading ? (
+              <p className="text-center text-xs sm:text-sm">loading...</p>
+            ) : (
+              <button
+                className="h-10 w-full rounded-lg border border-gray-400 text-xs text-gray-500 hover:bg-black hover:text-white sm:text-sm"
+                type="submit"
+              >
+                Create
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
