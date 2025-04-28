2868816f7c4606ced39685f8c01ccbf79f5df7ba
Agustin Alonso <alonsoiagustin@gmail.com>
2025-04-28 09:54:39 +0200

    Enhance Footer component with updated layout and link to GitHub for improved accessibility

--- frontend/src/components/shared/Footer.tsx
+++ frontend/src/components/shared/Footer.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
-    <footer className="flex w-full items-center justify-center gap-4 bg-red-50 pr-4 pl-4 leading-10">
-      <p>&copy; {new Date().getFullYear()} Wallaclone</p>
+    <footer className="flex w-full flex-row items-center justify-center gap-4 border-t border-t-gray-400 bg-gray-300 pr-4 pl-4 text-sm leading-10 sm:text-base">
+      <Link to="https://github.com/KeepcodersWeb17 " target="_blank">
+        <p>&copy; {new Date().getFullYear()} Wallaclone</p>
+      </Link>
       <p>All rights reserved.</p>
     </footer>
   );
