a83be112d61d7e33bd5e056418a97f3fa4ff3745
Agustin Alonso <alonsoiagustin@gmail.com>
2025-05-02 22:41:42 +0200

    Refactor HomePage: remove unnecessary whitespace in published by labels

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import type { Advert, Tag } from "../store/state/types";
-// import SearchIcon from "../components/icons/Search";
 import UnlikeIcon from "../components/icons/Unlike";
 import LikeIcon from "../components/icons/Like";
 import { getAdverts, getTags, getUser } from "../store/selectors/selectors";
@@ -106,8 +105,7 @@ const HomePage = () => {
         {/* Search form */}
         <section className="flex w-full flex-grow flex-col items-center justify-center gap-4">
           <h2 className="text-xl leading-15 font-bold sm:text-2xl md:text-3xl">
-            {" "}
-            What are you looking for?{" "}
+            What are you looking for?
           </h2>
           <div className="flex w-full flex-col items-center justify-center gap-8">
             <form
@@ -207,7 +205,7 @@ const HomePage = () => {
                   </div>
                   <div className="">
                     <p>
-                      Published by:{" "}
+                      Published by:
                       <Link to={`/adverts/user/${advert.owner?.username}`}>
                         <strong>{advert.owner.username}</strong>
                       </Link>
@@ -278,7 +276,7 @@ const HomePage = () => {
                     </div>
                     <div className="">
                       <p>
-                        Published by:{" "}
+                        Published by:
                         <Link to={`/adverts/user/${advert.owner?.username}`}>
                           <strong>{advert.owner.username}</strong>
                         </Link>
@@ -351,7 +349,7 @@ const HomePage = () => {
                     </div>
                     <div className="">
                       <p>
-                        Published by:{" "}
+                        Published by:
                         <Link to={`/adverts/user/${advert.owner?.username}`}>
                           <strong>{advert.owner.username}</strong>
                         </Link>
