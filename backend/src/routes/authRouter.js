2d65d6a14de06e0d6b1f96b8959db16ec148f533
Agustin Alonso <alonsoiagustin@gmail.com>
2025-05-05 11:20:24 +0200

    Import recoveryPassword controller in authRouter for password recovery functionality

--- backend/src/routes/authRouter.js
+++ backend/src/routes/authRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { login, logout } from "../controllers/authController.js";
 import { validateLogin as validateUser } from "../middlewares/validateMiddleware.js";
+import { recoveryPassword } from "../controllers/authController.js";
 
 const router = express.Router();
 
